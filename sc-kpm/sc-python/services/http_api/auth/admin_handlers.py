ad63d72d7898a09216d7b9fbd65cf4fe30b8ea5d
Shtress <stas.butrin@mail.ru>
2022-03-21 16:42:44 +0300

    [http_api] Add kb user

--- sc-kpm/sc-python/services/http_api/auth/admin_handlers.py
+++ sc-kpm/sc-python/services/http_api/auth/admin_handlers.py
@@ -1,4 +1,8 @@
+import asyncio
 import json
+
+import websockets
+from tornado import websocket
 
 from http_api.auth import constants as cnt
 from http_api.auth.base_handler import BaseHandler
@@ -6,7 +10,7 @@ from http_api.auth.common import get_response_message
 from http_api.auth.database import DataBase
 from http_api.auth.validators import TokenValidator, TokenType
 from http_api.auth.verifiers import username_verifier
-
+from sc import *
 
 def _verify_user_info_in_database(database: DataBase, name: str, password: str) -> str:
     if not username_verifier.verify(name):
@@ -27,6 +31,176 @@ def _check_if_user_in_base(database: DataBase, name: str) -> str:
     return message_desc
 
 
+async def create_kb_user(username: str):
+    async with websockets.connect('ws://localhost:8090/ws_json') as ws:
+        payload = [
+                {
+                    "command": "find",
+                    "idtf": "nrel_login"
+                },
+                {
+                    "command": "find",
+                    "idtf": "ui_user"
+                }
+        ]
+        resolve_ui_user = {"id": 1, "type": "keynodes", "payload": payload}
+        await ws.send(json.dumps(resolve_ui_user))
+        response = await ws.recv()
+        login_addr, ui_user_addr = json.loads(response)["payload"]
+        print(login_addr)
+        print(ui_user_addr)
+        payload = []
+        payload.append({
+            'el': 'link',
+            'type': ScType.LinkConst.ToInt(),
+            'content' : username
+            })
+        payload.append({
+            'el': 'node',
+            'type': ScType.NodeConst.ToInt()
+            })
+        payload.append({
+            'el': 'edge',
+            'src': {'type': 'ref', 'value': 1},
+            'trg': {'type': 'ref', 'value': 0},
+            'type': ScType.EdgeDCommonConst.ToInt()
+            })
+        payload.append({
+            'el': 'edge',
+            'src': {'type': 'addr', 'value': login_addr},
+            'trg': {'type': 'ref', 'value': 2},
+            'type': ScType.EdgeAccessConstPosPerm.ToInt()
+            })
+        payload.append({
+            'el': 'edge',
+            'src': {'type': 'addr', 'value': ui_user_addr},
+            'trg': {'type': 'ref', 'value': 1},
+            'type': ScType.EdgeAccessConstPosPerm.ToInt()
+            })
+        message = {"id": 2 ,"type": "create_elements", "payload": payload}
+        print(message)
+        await ws.send(json.dumps(message))
+        print(await ws.recv())
+
+async def delete_kb_user(username: str):
+    async with websockets.connect('ws://localhost:8090/ws_json') as ws:
+        payload = [
+                {
+                    "command": "find",
+                    "idtf": "nrel_login"
+                },
+                {
+                    "command": "find",
+                    "idtf": "ui_user"
+                }
+        ]
+        resolve_ui_user = {"id": 1, "type": "keynodes", "payload": payload}
+        await ws.send(json.dumps(resolve_ui_user))
+        response = await ws.recv()
+        login_addr, ui_user_addr = json.loads(response)["payload"]
+        payload = [
+                {
+                    "command": "find",
+                    "data": username
+                }
+                ]
+        get_login_links = {"id": 2, "type": "content", "payload": payload}
+        await ws.send(json.dumps(get_login_links))
+        response = await ws.recv()
+        print(response)
+        links = json.loads(response)['payload']
+
+        for link in links[0]:
+            templ = []
+            triple = []
+
+
+            triple.append(
+                    {
+                    "type": "addr",
+                    "value": ui_user_addr
+                    })
+            triple.append({
+                    "type": "type",
+                    "value": ScType.EdgeAccessVarPosPerm.ToInt(),
+                    "alias": "_user_edge"
+                    })
+            triple.append(
+                    {
+                    "type": "type",
+                    "value": ScType.NodeVar.ToInt(),
+                    "alias": "_user"
+                    }
+                    )
+            templ.append(triple)
+
+            triple = []
+            triple.append(
+                    {
+                    "type": "alias",
+                    "value": "_user"
+                    })
+            triple.append(
+                    {
+                    "type": "type",
+                    "value": ScType.EdgeDCommonVar.ToInt(),
+                    "alias": "_link_edge"
+                    })
+            triple.append(
+                    {
+                    "type": "addr",
+                    "value": link,
+                    "alias": "_link"
+                    })
+            templ.append(triple)
+
+            triple = []
+            triple.append(
+                    {
+                    "type": "addr",
+                    "value": login_addr,
+                    })
+            triple.append(
+                    {
+                    "type": "type",
+                    "value": ScType.EdgeAccessVarPosPerm.ToInt(),
+                    "alias": "_login_edge"
+                    })
+            triple.append(
+                    {
+                    "type": "alias",
+                    "value": "_link_edge"
+                    })
+            templ.append(triple)
+
+            print(templ)
+            payload = {
+                        "templ": templ
+                      }
+            print(payload)
+            template_search = {"id": 3, "type": "search_template", "payload": templ}
+            await ws.send(json.dumps(template_search))
+            response = json.loads(await ws.recv())['payload']
+            print(response)
+            aliases = response['aliases']
+            print(aliases.values())
+            addrs = response['addrs']
+            print(addrs[0])
+
+            payload = []
+
+            for value in aliases.values():
+                print(addrs[0][value])
+                payload.append(addrs[0][value])
+
+            delete_elements = {"id": 4, "type": "delete_elements", "payload": payload}
+            await ws.send(json.dumps(delete_elements))
+            response = json.loads(await ws.recv())
+            print(response)
+
+
+
+
 class UserHandler(BaseHandler):
     @TokenValidator.validate_typed_token(TokenType.ACCESS)
     def post(self) -> None:
@@ -41,6 +215,8 @@ class UserHandler(BaseHandler):
         response = get_response_message(msg_desc)
         if msg_desc == cnt.MSG_ALL_DONE:
             database.add_user(**request_params)
+            loop = asyncio.get_event_loop()
+            loop.create_task(create_kb_user(request_params[cnt.NAME]))
         self.write(response)
 
     @TokenValidator.validate_typed_token(TokenType.ACCESS)
@@ -54,6 +230,8 @@ class UserHandler(BaseHandler):
                 cnt.ID: human_info[cnt.ID],
                 cnt.NAME: human_info[cnt.NAME]
             })
+
+
         else:
             response = get_response_message(cnt.MSG_USER_NOT_FOUND)
         self.write(response)
@@ -68,6 +246,8 @@ class UserHandler(BaseHandler):
             response = get_response_message(cnt.MSG_USER_NOT_FOUND)
         else:
             response = get_response_message(cnt.MSG_ALL_DONE)
+            loop = asyncio.get_event_loop()
+            loop.create_task(delete_kb_user(""))
         self.write(response)
 
     @TokenValidator.validate_typed_token(TokenType.ACCESS)
